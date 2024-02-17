dd8789637379d3d079e2ee44816fd45c69b99fa9
callaghan <hattinghcallaghan@gmail.com>
2024-02-17 14:34:07 +1000

    update argo

---
+++ argocd-install/README.md
@@ -0,0 +1,15 @@
+Requirements:
+* terraform 
+* minikube/kind
+* kubectl
+* helm
+
+```shell
+minikube start
+```
+To check context and that mk is running
+```shell
+kubectl get nodes
+```
+bootstrap Argocd
+
---
+++ argocd-install/argocd/apply.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update the dependencies for the Helm chart
+echo "Updating Helm chart dependencies..."
+helm dependency update
+
+# Create the 'argocd' namespace if it doesn't exist, using a dry run to generate the YAML, and then apply it
+echo "Ensuring the 'argocd' namespace exists..."
+kubectl create namespace argocd --dry-run=client --output=yaml | kubectl apply -f -
+
+# Deploy Argo CD using Helm, including CRDs, within the 'argocd' namespace
+echo "Deploying Argo CD to the 'argocd' namespace..."
+helm template argocd . --include-crds --namespace argocd | kubectl apply -n argocd -f -
+
+echo "Deployment completed."
+
